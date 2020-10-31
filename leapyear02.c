6c3d39c49074b20aec8d679f23f043d8cdd7906e
Axel Napolitano <napolitano@users.noreply.github.com>
2020-10-31 16:18:04 +0100

    Initial commit

---
+++ leapyear02.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define APP_BANNER         "\n\n-=> Schaltjahr oder nicht? <=\n\n"
+#define INPUT_PROMPT       "Bitte geben Sie eine Jahreszahl ein: "
+#define IS_A_LEAP_YEAR     "ist ein Schaltjahr"
+#define IS_NOT_A_LEAP_YEAR "ist kein Schaltjahr"
+
+/**
+ * Returns true, if leap year and otherwise false
+ *
+ * If the year is divisible by 4, the year is a leap year. 
+ * This rule has an exception: If the year is divisible by 100, the year is not a leap year. 
+ * This exception has again an exception: If the year is divisible by 400, the year is a leap year.
+ */
+bool isLeapJahr(int year) {
+    if(year % 400 == 0) {
+        return true; 
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Returns a simply colored app header
+ */
+void printHeader() {
+    printf(ANSI_COLOR_CYAN);
+    printf(APP_BANNER);
+    printf(ANSI_COLOR_RESET);
+}
+
+/**
+ * Gets the year from the user
+ */
+int getYearFromUser() {
+    int year = 0;
+    
+    printf(ANSI_COLOR_BLUE);
+    printf(INPUT_PROMPT);
+    printf(ANSI_COLOR_RESET);
+    printf("\n");
+    
+    scanf("%d", &year);
+
+    return year;
+}
+
+/**
+ * The main function
+ */
+int main () {
+
+    char *result = malloc(20);
+    char *fontColor = malloc(8);
+    
+    strcpy(result, IS_NOT_A_LEAP_YEAR);
+    strcpy(fontColor, ANSI_COLOR_RED);
+
+    printHeader();
+
+    int year = getYearFromUser();
+
+    if(isLeapJahr(year)) {
+        strcpy(result, IS_A_LEAP_YEAR);
+        strcpy(fontColor, ANSI_COLOR_GREEN);
+    } 
+
+    printf("\n%s%d %s%s%s\n", ANSI_COLOR_YELLOW, year, fontColor, result, ANSI_COLOR_RESET);
+
+    free(fontColor);
+    free(result);
+
+    return 0;
+}
