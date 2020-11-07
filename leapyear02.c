25021e4da12d3201fad349f766a737a9d2b75871
Axel Napolitano <napolitano@users.noreply.github.com>
2020-11-07 13:30:47 +0100

    Update leapyear02.c

--- leapyear02.c
+++ leapyear02.c
@@ -25,11 +25,11 @@
  * This exception has again an exception: If the year is divisible by 400, the year is a leap year.
  */
 bool isLeapYear(int year) {
-    if(year % 400 == 0) {
+    if(!(year % 400)) {
         return true; 
-    } else if (year % 100 == 0) {
+    } else if (!(year % 100)) {
         return false;
-    } else if (year % 4 == 0) {
+    } else if (!(year % 4)) {
         return true;
     } else {
         return false;
